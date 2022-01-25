f6e11a0d6367c77fc69b69ad59437e5a90b88dd4
kmwangemi <mwangemik@gmail.com>
2022-01-25 10:51:59 +0300

    add head

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f7b2b8a22b2f8067e949"; }
+/******/ 		__webpack_require__.h = function() { return "dc4be65c3455c8691c71"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
 import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
@@ -8,17 +10,31 @@ import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
 const Home = () => {
+
+   useEffect(() => {
+      window.scrollTo(0, 0)
+   }, [])
+
    return (
-      <Layout>
-         <Section grid >
-         <Hero />
-         <BgAnimation />
-         </Section>
-         <Projects />
-         <Technologies />
-         <Timeline />
-         <Acomplishments />
-      </Layout>
+      <>
+         <Head>
+            <title>Kelvin - Portfolio</title>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+         </Head>
+         <Layout>
+            <Section grid >
+               <Hero />
+               <BgAnimation />
+            </Section>
+            <Projects />
+            <Technologies />
+            <Timeline />
+            <Acomplishments />
+         </Layout>
+      </>
    );
 };
 
